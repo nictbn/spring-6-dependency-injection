6a384b34cb3d594a6fbca87579f29272bf64f3c6
Nicu Taban <nictbndeveloper@gmail.com>
2023-11-26 11:22:01 +0200

    Initial commit

---
+++ src/main/java/com/example/spring6dependencyinjection/Spring6DependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring6dependencyinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6DependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6DependencyInjectionApplication.class, args);
+	}
+
+}

2df710a5160309e56c437e8d9ee8c19cf3278095
Nicu Taban <nictbndeveloper@gmail.com>
2023-11-26 16:53:38 +0200

    Using @Ualifier to inject the GreetingServicePropertyInjected into the PropertyInjectedController

--- src/main/java/com/example/spring6dependencyinjection/controllers/PropertyInjectedController.java
+++ src/main/java/com/example/spring6dependencyinjection/controllers/PropertyInjectedController.java
@@ -2,10 +2,12 @@ package com.example.spring6dependencyinjection.controllers;
 
 import com.example.spring6dependencyinjection.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
+    @Qualifier("greetingServicePropertyInjected")
     @Autowired
     GreetingService greetingService;
 

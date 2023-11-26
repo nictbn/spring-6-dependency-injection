e2beb54388ac14037feb0d15b870c647eecbb32a
Nicu Taban <nictbndeveloper@gmail.com>
2023-11-26 16:50:11 +0200

    Using the @Qualifier to pick the base greeting service

--- src/main/java/com/example/spring6dependencyinjection/controllers/ConstructorInjectedController.java
+++ src/main/java/com/example/spring6dependencyinjection/controllers/ConstructorInjectedController.java
@@ -1,13 +1,14 @@
 package com.example.spring6dependencyinjection.controllers;
 
 import com.example.spring6dependencyinjection.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
-    public ConstructorInjectedController(GreetingService greetingService) {
+    public ConstructorInjectedController(@Qualifier("greetingServiceImpl") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 

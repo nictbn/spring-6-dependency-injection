bece7de3c13e5fdcca66b10e8741804db6370c17
Nicu Taban <nictbndeveloper@gmail.com>
2023-11-26 11:57:53 +0200

    Spring test context example

--- src/test/java/com/example/spring6dependencyinjection/Spring6DependencyInjectionApplicationTests.java
+++ src/test/java/com/example/spring6dependencyinjection/Spring6DependencyInjectionApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.spring6dependencyinjection;
 
+import com.example.spring6dependencyinjection.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DependencyInjectionApplicationTests {
 
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testGetControllerFromContext() {
+		MyController controller = applicationContext.getBean(MyController.class);
+		System.out.println(controller.sayHello());
+	}
+
+	@Test
+	void testAutowiredController() {
+		System.out.println(myController.sayHello());
 	}
 
 }
